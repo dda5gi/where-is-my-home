f0ab015e8e8f063e66bf5a2af7287a6356b71aa6
dobbi030 <dmstjs7047@hanmail.net>
2022-11-24 06:23:14 +0900

    feat: Show MemberInfo on Main page

--- whereismyhome_final_fe/src/views/AppMain.vue
+++ whereismyhome_final_fe/src/views/AppMain.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <v-parallax :src="require(`@/assets/home_banner.jpg`)">
-      <h1 class="text-h4 font-weight-thin mb-4">Where Is My Home</h1>
+      <h1 class="text-h4 font-weight-thin mb-4" v-if="!isLogin">Welcome, Guest</h1>
+      <h1 class="text-h4 font-weight-thin mb-4" v-else>
+        Welcome, <b>{{ this.memberInfo.name }}[{{ this.memberInfo.id }}]</b>
+      </h1>
     </v-parallax>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
+const memberStore = "memberStore";
+
 export default {
   name: "AppMain",
+
+  computed: {
+    ...mapState(memberStore, ["isLogin", "memberInfo"]),
+  },
 };
 </script>
 
