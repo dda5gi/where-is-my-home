c59487e46da72691cf51d48c90ad0dc6b0323674
dobbi030 <dmstjs7047@hanmail.net>
2023-01-03 16:18:30 +0900

    git lfs 적용
    Former-commit-id: 05bec48ed1deb9fca3becac3865125910a78eaa9

--- whereismyhome_final_be/bin/src/main/resources/dbschema/final_table.sql
+++ whereismyhome_final_be/bin/src/main/resources/dbschema/final_table.sql
@@ -1,18 +1,3 @@
-use ssafy_final;
-
-CREATE TABLE IF NOT EXISTS `ssafy_final`.`member` (
-  `userid` VARCHAR(16) NOT NULL,
-  `username` VARCHAR(20) NOT NULL,
-  `userpwd` VARCHAR(16) NOT NULL,
-  `joindate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `token` VARCHAR(1000) NULL DEFAULT NULL,
-  PRIMARY KEY (`userid`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-insert into member
-values ('ssafy', '김싸피', 'ssafy', now(), null);
-
-insert into member
-values ('admin', '관리자', 'admin', now(), null);
+version https://git-lfs.github.com/spec/v1
+oid sha256:06a6c6eec0d3bc7ae3353819a349c4199743a1c0e44fbada2df51a7d0c7b887a
+size 540
---
+++ whereismyhome_final_be/src/main/resources/dbschema/final_db.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:18d9c35ba804009b45b41fd4ee44b07715156420d300c0716036b105d209198a
+size 313423889
